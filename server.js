d048685adccf6164d19a83c8e2f4c3f2ccb56d22
Elayas569 <ortegaaguilarelias@gmail.com>
2025-05-12 14:55:41 -0600

    fixv5

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ const conectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { notFound } = require("./middleware/notFoundMiddleware");
+const cors = require("cors"); // Importar CORS
 
 conectDB();
 
 const app = express();
 
+const corsOptions = {
+  origin:
+    "https://ordinario-web-front-qqevua2ov-elayas-projects-d13d6d3e.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Rutas de la API
 app.use("/api/casas", require("./routes/casaRoutes"));
-//app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 
 app.use(notFound);
